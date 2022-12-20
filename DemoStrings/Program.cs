c88ad8fc4b06f2f659ba51aa7afaadc4fb0fa90a
Prasiddha Shrestha <prasiddhastha7@gmail.com>
2022-12-20 22:29:09 +0545

    Add project files.

---
+++ DemoStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace DemoStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var firstName = "Prasiddha";
+            var lastName = "Shrestha";
+            //concatenation
+            var fullName = firstName + " " + lastName;
+            //format method
+            var myFullName = string.Format("{0} {1}", firstName, lastName);
+            var names = new string[3] { "prasi", "khushbu", "shrestha" };
+            //join method
+            var joinName = string.Join(",", names);
+            Console.WriteLine(joinName);//join method
+            Console.WriteLine(fullName);//concatenation
+            Console.WriteLine(myFullName);//format method
+        }
+    }
+}
